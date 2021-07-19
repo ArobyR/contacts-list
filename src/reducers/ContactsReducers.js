7ab203e1387fe9dab2afd8a01a9fb5e194ebdb79
ArobyR <ar.rosario001@gmail.com>
2021-07-19 15:12:59 -0400

    add delete action

--- src/reducers/ContactsReducers.js
+++ src/reducers/ContactsReducers.js
@@ -1,9 +1,11 @@
-export const ContactsReducer = (state, action) => {
-    switch (action.type) {
-        case "add":
-            return [...state, action.payload]
+export const ContactsReducer = (state = [], action) => {
+  switch (action.type) {
+    case "add":
+      return [...state, action.payload];
+    case "delete":
+      return state.filter((current) => current.id !== action.payload);
 
-        default:
-            return state;
-    }
-}
+    default:
+      return state;
+  }
+};
